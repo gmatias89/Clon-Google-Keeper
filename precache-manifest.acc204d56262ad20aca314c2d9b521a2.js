7409e9916dea241164a21652e59f1bf944a57d6a
Matias Milla <gmatias89@hotmail.com>
2020-04-24 17:19:11 -0300

    Updates

---
+++ precache-manifest.acc204d56262ad20aca314c2d9b521a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b3c8cdedcd72532dfd62e15bba049dd",
+    "url": "/Clon-Google-Keeper/index.html"
+  },
+  {
+    "revision": "8dbe1f8e0de582c47a3f",
+    "url": "/Clon-Google-Keeper/static/js/2.fdc06cda.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Clon-Google-Keeper/static/js/2.fdc06cda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ddb55f1fc3a4e4335f8",
+    "url": "/Clon-Google-Keeper/static/js/main.1c316fc7.chunk.js"
+  },
+  {
+    "revision": "09ac0c81f310726ede9a",
+    "url": "/Clon-Google-Keeper/static/js/runtime-main.fcbed4ca.js"
+  }
+]);
